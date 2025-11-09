967c343319ff95e6530094fef106d21dc1eb5500
Hanz Dela Cruz <Hanztheprince@gmail.com>
2025-11-09 11:54:45 -0700

    aws stuff

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,8 +1,8 @@
 // src/lib/supabaseClient.js
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://yfzlvocdvrrplvrkhokh.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlmemx2b2NkdnJycGx2cmtob2toIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI2MjQwNzMsImV4cCI6MjA3ODIwMDA3M30.Ky7RALAi4Vt2DhxYBYRmTOivrFLW9kQ4-ftjG_-Oz60';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
